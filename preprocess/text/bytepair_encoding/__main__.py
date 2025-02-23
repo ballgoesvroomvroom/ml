03a31be1acd4f5ae7adc2d8e45c4497cdbdfb49a
Chong Cheng Hock <54146032+ballgoesvroomvroom@users.noreply.github.com>
2025-02-23 11:48:43 +0000

    add corpus

--- preprocess/text/bytepair_encoding/__main__.py
+++ preprocess/text/bytepair_encoding/__main__.py
@@ -13,7 +13,9 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.stem import WordNetLemmatizer
 
 nltk.download("stopwords")
+nltk.download("punkt_tab")
 nltk.download("wordnet")
+
 
 def pre_tokenize(doc):
 	# doc: string
@@ -22,14 +24,20 @@ def pre_tokenize(doc):
 	doc = doc.lower()
 
 	# split into tokens
-	tokens = [token for sent in sent_tokenize(doc) for token in word_tokenize(sent)]
+	tokens = [
+	    token for sent in sent_tokenize(doc) for token in word_tokenize(sent)
+	]
 
 	# remove punctuations
 	non_alphabetical = re.compile("[^a-zA-Z]")
-	stripped_punct_tokens = [non_alphabetical.sub("", token) for token in tokens]
+	stripped_punct_tokens = [
+	    non_alphabetical.sub("", token) for token in tokens
+	]
 
 	# remove tokens with len() <= 2 (character count)
-	min_twochar_tokens = [token for token in stripped_punct_tokens if len(token) >= 3]
+	min_twochar_tokens = [
+	    token for token in stripped_punct_tokens if len(token) >= 3
+	]
 
 	# lemmatise tokens
 	lemmatiser = WordNetLemmatizer()
@@ -39,17 +47,18 @@ def pre_tokenize(doc):
 
 
 class BytePairEncoder:
+
 	def __init__(self):
 		pass
 
 	def common_pairs(self):
 		counter = {}
 		for idx, char in enumerate(self.chars):
-			if (idx >= len(self.chars) -1):
+			if (idx >= len(self.chars) - 1):
 				# last one ignore
 				continue
-			
-			pair = (char, self.chars[idx +1])
+
+			pair = (char, self.chars[idx + 1])
 			if (pair in counter):
 				counter[pair] += 1
 			else:
@@ -59,21 +68,20 @@ class BytePairEncoder:
 	def fit(self, tokens):
 		# tokens: str[], pre-processed tokens
 		# obtain characters
-		self.chars = [char for token in tokens for char in token]
+		self.chars = [char for token in tokens for char in str(token)]
 
 		# build vocab characters
 		self.vocab = set(self.chars)
 
 		# find merge rules
 		self.merge_rules = []
-		for i in range(10):
+		for i in range(200):
 			# 10 iterations
 			pairs = self.common_pairs()
 
 			# get max token
-			most_common_pair = None # key element of return value (dict) self.common_pairs()
+			most_common_pair = None  # key element of return value (dict) self.common_pairs()
 			for pair, freq in pairs.items():
-				print(pair, freq)
 				if (most_common_pair and freq >= pairs[most_common_pair]):
 					most_common_pair = pair
 				elif (most_common_pair is None):
@@ -87,19 +95,37 @@ class BytePairEncoder:
 			new_chars = []
 			local_context = None
 			for idx, char in enumerate(self.chars):
-				if (local_context is None):
-					if (char == most_common_pair[0]):
-						local_context = char
-					else:
-						new_chars.append(char)
-				elif (char == most_common_pair[1]):
-					# pair found
-					new_chars.append(local_context +char)
-					local_context = None # reset state
-			self.chars = new_chars # set chars
+				if (char == most_common_pair[0]):
+					# start of a pair
+					if (local_context):
+						new_chars.append(local_context)
+					local_context = char
+				elif (char == most_common_pair[1]
+				      and local_context is not None):
+					# end of a pair
+					new_chars.append(local_context + char)
+					local_context = None  # reset state
+				else:
+					# not a pair
+					if (local_context):
+						new_chars.append(local_context)
+						local_context = None  # reset state
+					new_chars.append(char)
+			self.chars = new_chars  # set chars
 
 		return self.chars
 
-tokens = pre_tokenize("Hello world. Mrs. Smith wants some granny apples. Please get her some apples")
+
+corpus = pd.read_csv(
+    "preprocess/text/bytepair_encoding/training_corpus/sentences.txt",
+    sep="\t",
+    names=["index", "text"])
+print(corpus)
+tokens = list(corpus["text"].apply(pre_tokenize).explode())
 bpe = BytePairEncoder()
 print(bpe.fit(tokens))
+# tokens = pre_tokenize("Hello world. Mrs. Smith wants some granny apples. Please get her some apples")
+# # tokens = pre_tokenize("Mrs. Smith wants some granny apples.")
+# print(tokens)
+# bpe = BytePairEncoder()
+# print(bpe.fit(tokens))
